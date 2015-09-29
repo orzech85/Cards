131c163644397342690bf5681d2b80ef2806e51c
Michal Orzechowski <michal@orzechowski.info.pl>
2015-09-29 23:39:14 +0200

    CardXmlGenerator added

---
+++ CardsXmlGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CardsXmlGenerator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length == 2)
+			{
+
+				string[] lines = File.ReadAllLines(args[0]);
+
+				string output = "<cards>";
+
+				foreach (var item in lines)
+				{
+					string[] items = item.Split(';');
+					output += "<card><native><item type=\"master\">" + items[0] + "</item></native>" +
+						"<foreign><item type=\"master\">" + items[1] + "</item></foreign></card>";
+				}
+
+				output += "</cards>";
+
+				File.WriteAllText(args[1], output);
+
+			} else
+			{
+				Console.WriteLine("zla liczba parametrow");
+			}
+		}
+	}
+}
